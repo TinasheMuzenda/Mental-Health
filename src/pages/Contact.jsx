405eb6fc276197bff558c16fee9361f6ec48af98
TinasheMuzenda <tmuzendat@gmail.com>
2024-10-27 22:42:46 +0200

    Navbar and Footer in Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Contact = () => {
   return (
     <main>
-      contact
+      <Navbar/>
+      
+      <Footer/>
     </main>
   )
 }
